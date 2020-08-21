27e711fbf97c440124be543eb73d0b78523171a2
unknown <uesugi.chesster@gmail.com>
2020-08-22 04:21:56 +0700

    add texthelper

--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 # just collection of code I just learn
 
-## tictactoe
+## [tictactoe](https://matosummer.github.io/learn-playground/tictactoe/)
 
 Simple XO game with vuejs
+
+## [texthelper](https://matosummer.github.io/learn-playground/texthelper/)
+
+Add something in front or back your list of text
---
+++ texthelper/app.js
@@ -0,0 +1,19 @@
+const ORIGINALTEXT = document.querySelector("#originalText");
+const RESULTTEXT = document.querySelector("#resultText");
+const BEFORETEXT = document.querySelector("#beforeText");
+const AFTERTEXT = document.querySelector("#afterText");
+
+function convertText() {
+  let textLine = ORIGINALTEXT.value.split("\n");
+  let before = BEFORETEXT.value ? BEFORETEXT.value : "";
+  let after = AFTERTEXT.value ? AFTERTEXT.value : "";
+  RESULTTEXT.textContent = before + textLine[0] + after;
+  for (let i = 1; i < textLine.length; i++) {
+    RESULTTEXT.textContent += "\r\n" + before + textLine[i] + after;
+  }
+}
+
+function highlightTextArea(e) {
+  e.focus();
+  e.select();
+}
